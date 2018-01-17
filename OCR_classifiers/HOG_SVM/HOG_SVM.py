e30090bd30044041f4c61ac1113876a8189b1dd7
XimingFeng <xfeng36@asu.edu>
2018-01-17 11:18:39 -0700

    fix HOG_SVM init

--- OCR_classifiers/HOG_SVM/HOG_SVM.py
+++ OCR_classifiers/HOG_SVM/HOG_SVM.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 class classifier_HOG_SVM(object):
 
@@ -13,7 +14,6 @@ class classifier_HOG_SVM(object):
         self.img = imgs
         self.meta = meta
         num_total = len(self.meta)
-        print("Total number of images is ", num_total)
         self.num_train = num_train
         self.num_val = num_val
         self.num_test = num_total - num_train
@@ -25,7 +25,47 @@ class classifier_HOG_SVM(object):
         self.y_val = self.y_train[selected_val_index]
         selected_test_index = np.delete(np.arange(num_total), selected_train_index)
         self.X_test = self.img[:, :, selected_test_index]
-        self.y_test = self.y_train[selected_test_index]
+        self.y_test = self.meta[selected_test_index]
+
+    def data_preprocessing(self, img):
+        """
+        convert raw images into HOG vectors
+        :param img: 200 * 200 * num_train binarized image
+        :return: HOG vectors for images
+        """
+        window_size = (200, 200)
+        block_size = (20, 20)
+        block_stride = (2, 2)
+        cell_size = (10, 10)
+        n_bins = 9
+        derive_aperture = 1
+        wind_sigma = -1
+        histogram_norm_type = 0
+        l2_hys_threshold = 0.2
+        gamma_correction = 1
+        n_levels = 64
+        signed_gradients = True
+        hog = cv2.HOGDescriptor(
+            window_size,
+            block_size,
+            block_stride,
+            cell_size,
+            n_bins,
+            derive_aperture,
+            wind_sigma,
+            histogram_norm_type,
+            l2_hys_threshold,
+            gamma_correction,
+            n_levels,
+            signed_gradients
+        )
+        img_hog = []
+        for img_idx in range(self.num_train):
+            img = self.X_train[:, :, img_idx]
+            descriptor = hog.compute(img)
+            img_hog.append(descriptor)
+        img_hog = np.dstack(img_hog)
+        return img_hog
 
 
 
